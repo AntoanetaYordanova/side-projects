3897b69bd6fc1d86e1509f9e76c4967172015022
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2021-04-03 10:55:19 +0200

    practice

---
+++ functionsPractice.js
@@ -0,0 +1,14 @@
+let arrOne = [1,2,3,4,5,6];
+let arrTwo = [1,2,3];
+
+function isIndexCorrect(arr, index){
+    if(index < 0 || index > arr.length - 1){
+        return false;
+    } else{
+        return true;
+    }
+}
+
+let isCorrect = isIndexCorrect(arrTwo, 5);
+
+console.log(isCorrect);
