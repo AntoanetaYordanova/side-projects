5f720a5e5eb5b06ed2e338a0ae7fe2cdd391d775
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2021-04-12 15:38:34 +0200

    practice

--- practice.js
+++ practice.js
@@ -1,14 +1,9 @@
-function aggregate(start, end, func) {
-    for (var result = start, i = start+1; i <= end; i++)
-      result = func(result, i);
-    return result;
-  }
-  
-  let sum1to10 = aggregate(1, 10, (a, b) => a + b);
-  console.log(sum1to10);
-  
-  let mult1to10 = aggregate(1, 10, (a, b) => a * b);
-  console.log(mult1to10);
-  
-  let digits1to10 = aggregate(1, 10, (a, b) => '' + a + b);console.log(digits1to10);
+let myArray = ['1, 2, 3, 4, 5'];
+
+
+let newArr = myArray[0];
+
+newArr = newArr.split(',')
+
+console.log(newArr);
   
