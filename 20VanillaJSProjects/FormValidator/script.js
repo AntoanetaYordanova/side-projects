1026c784b8444eb3a797ecff7142c40d82210ec1
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2021-08-15 22:12:14 +0200

    JS Exercises

---
+++ 20VanillaJSProjects/FormValidator/script.js
@@ -0,0 +1,50 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+
+// Event listeners
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    
+    if (username.value === '') {
+        showError(username, 'Username is required');
+    } else {
+        showSuccess(username);
+    }
+
+    if(password.value === '') {
+        showError(password, 'Password is required')
+    } else {
+        showSuccess(password);
+    }
+
+
+    if(password2.value === '') {
+        showError(password2, 'Password 2 is required')
+    } else {
+        showSuccess(password2);
+    }
+
+    if(email.value === '') {
+        showError(email, 'Email is required')
+    } else {
+        showSuccess(email);
+    }
+});
