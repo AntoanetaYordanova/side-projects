d0219d8626dc8d2458c2ac5109e23ff49b7a2910
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2021-08-27 21:23:46 +0200

    Exercise

--- Advanced/DOM Introduction/exercise/05. Table - Search Engine/solution.js
+++ Advanced/DOM Introduction/exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,19 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const allTD = [...document.getElementsByTagName('td')];
+      let findString = document.getElementById('searchField').value;
 
+      for(let i = 1; i < allTD.length; i++) {
+         const td = allTD[i];
+         console.log(td);
+
+         if(td.textContent.includes(findString)) {
+            td.parentElement.classList.add('select');
+
+         } 
+      }
+      
+      document.getElementById('searchField').value = ''
    }
 }
