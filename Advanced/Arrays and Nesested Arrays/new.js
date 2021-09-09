f773496570e31fa92ba3fd441263dbf7e1a0c005
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2021-09-09 11:07:16 +0200

    Exercise

--- Advanced/Arrays and Nesested Arrays/new.js
+++ Advanced/Arrays and Nesested Arrays/new.js
@@ -1,10 +1,13 @@
-const printEl = row => row.forEach(el => console.log(el))
+const department = {
+    name : 'Engineering',
+    data : {
+        director : {
+            name : 'John',
+            position : 'Engineering Director'
+        } ,
+        employees : [],
+    }
+};
 
-function loop(arr) {
-    arr.forEach(printEl);
-}
-
-
-
-
-loop([[1, 2, 3, 4],[5, 6, 7, 8],[9, 8, 7, 6]])
+const {data : {director}} = department;
+console.log(director);
--- Advanced/Arrays and Nesested Arrays/printOne.js
+++ Advanced/Arrays and Nesested Arrays/printOne.js
@@ -1,3 +1,5 @@
-function printOne(params) {
-    
+function printOne(array, joiner) {
+console.log(array.join(joiner));
 }
+
+printOne(['One', 'Two', 'Three', 'Four', 'Five'], '-');
