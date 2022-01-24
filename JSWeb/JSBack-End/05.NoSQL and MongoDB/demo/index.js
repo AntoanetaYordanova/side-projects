5d98ea43d5fb2245c365e2364e284a21827f10c6
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2022-01-24 10:27:07 +0100

    JS Back End

--- JSWeb/JSBack-End/05.NoSQL and MongoDB/demo/index.js
+++ JSWeb/JSBack-End/05.NoSQL and MongoDB/demo/index.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const Cat = require('./models/Cat');
+const Person = require('./models/Person');
+
 
 start();
 
@@ -12,28 +15,10 @@ async function start() {
 
   console.log('Database connected');
 
-  const catSchema = new mongoose.Schema({
-    name: String,
-    color: String,
-  });
-
-  const Cat = mongoose.model('Cat', catSchema);
-
-  const personSchema = new mongoose.Schema({
-    firstName: { type: String, required : true},
-    lastName: { type: String, required : true},
-    age: { type: Number, required : true},
-  });
-
-  personSchema.virtual('fullName').get(function () {
-    return `${this.firstName} ${this.lastName}`;
-  });
-
-  personSchema.methods.sayHi = function () {
-    console.log(`I am ${this.firstName} and I am ${this.age} years old`);
-  };
-
-  const Person = mongoose.model('Person', personSchema);
+  const cat = new Cat({
+    name : 'Flufy',
+    color : 'Grey'
+  })
 
   const person1 = new Person({
     firstName: 'Peter',
@@ -41,20 +26,14 @@ async function start() {
     age: 23,
   });
 
-  const person2 = new Person({
-    firstName: 'Tony',
-    lastName: 'Yordanova',
-    age: 31,
-  });
-
-//   await person1.save();
-//   await person2.save();
+  // await cat.save();
+  // await person1.save();
 
   const data = await Person.find({});
 
-//   data.forEach((p) => p.sayHi());
+  data.forEach((p) => p.sayHi());
 
   data.map(p => p.fullName).forEach(n => console.log(n));
-
-//   console.log(data);
+  const person = await Person.find({firstName : 'Tony'});
+  console.log(person);
 }
