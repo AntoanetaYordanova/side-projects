6634a4f6c9d5bd3bfcb541df6da59113496b81d1
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2022-02-18 16:28:13 +0100

    JS Back End

---
+++ JSWeb/JSBack-End/framework/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const configExpress = require('./config/express');
+const configRoutes = require('./config/routes');
+const configDatabase = require('./config/database');
+
+start();
+
+async function start() {
+  const app = express();
+
+  configExpress(app);
+  await configDatabase();
+  configRoutes(app);
+
+  app.get('/', (req, res) => {
+    res.render('home');
+  });
+
+  app.listen(3000, () => console.log('Server running on port 3000'));
+}
---
+++ JSWeb/JSBack-End/framework/services/user.js
@@ -0,0 +1,50 @@
+
+const User = require('../models/User');
+const { hash, compare} = require('bcrypt');
+
+//TODO: add all required fields
+async function register(username, password) {
+    const existing = await(getUserByUsername(username));
+    if(existing) {
+        throw new Error('Username is taken');    
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    const user = new User({
+        username, 
+        hashedPassword
+    });
+    
+    await user.save();
+
+    return user;
+}
+
+//TODO: change identifier
+
+async function login(username, password) {
+    // console.log(username);
+    const user = await getUserByUsername(username);
+
+    if(!user) {
+        throw new Error('Wrong username or password');
+    }
+    const passwordMatch = await compare(password, user.hashedPassword);
+
+    if(!passwordMatch) {
+        throw new Error('Wrong username or password');
+    }
+
+    return user;
+}
+
+//TODO: find user by given identifier 
+async function getUserByUsername(username) {
+    return await User.findOne({ username : new RegExp (`^${username}$`, 'i')});
+}
+
+module.exports = {
+    login,
+    register
+}
