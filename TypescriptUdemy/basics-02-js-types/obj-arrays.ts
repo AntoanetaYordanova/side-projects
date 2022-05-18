ba8af68332fe58d14a1374af8c5f08894f14ea37
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2022-05-18 11:30:32 +0300

    Side projects

---
+++ TypescriptUdemy/basics-02-js-types/obj-arrays.ts
@@ -0,0 +1,18 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]
+// } = {
+//   name: 'Max',
+//   age: 23,
+//   hobbies: ['running', 'something else'],
+//   role: [2, 'author']
+// }
+
+const person = {
+    name: 'Max',
+    age: 23,
+    hobbies: ['running', 'something else'],
+    role: [2, 'author']
+  }
